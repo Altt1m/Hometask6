d799be74bb019acf80966bb4d662e45fd588a4fd
Viacheslav Melnyk <altt1m.zaskar@gmail.com>
2023-08-01 23:41:55 +0300

    Finish.

--- Program.cs
+++ Program.cs
@@ -53,14 +53,14 @@ namespace Hometask6
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-            } 
+            }
 
             // Task 6
             double candiesPrice, cookiesPrice, applesPrice;
             candiesPrice = 115.45; cookiesPrice = 78.90; applesPrice = 53.80;
             double x, y, z;
             x = 0.95; y = 1.3; z = 2.1;
-            double cheque = candiesPrice*x + cookiesPrice *y + applesPrice*z;
+            double cheque = candiesPrice * x + cookiesPrice * y + applesPrice * z;
             Console.WriteLine($"\nTo be paid: {cheque:F2}.\n");
 
             // Task 7
@@ -90,7 +90,7 @@ namespace Hometask6
             // Task 10
             a = new Random().Next(1, 100); b = new Random().Next(1, 100);
             Console.WriteLine(a); Console.WriteLine(b);
-            if (a>b)
+            if (a > b)
             {
                 Console.WriteLine($"{a}, {b}");
             }
@@ -116,7 +116,7 @@ namespace Hometask6
             {
                 secondLargest = Math.Max(b, c);
             }
-            Console.WriteLine($"Sum = {largest+secondLargest}.\n");
+            Console.WriteLine($"Sum = {largest + secondLargest}.\n");
 
             // Task 12
             double[] array = { 1, 2, 3, 4, 5 };
@@ -144,8 +144,211 @@ namespace Hometask6
             }
             PrintArray(array);
 
+            // Task 13
+            int[] prices = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            int suma = 0;
+            foreach (int i in prices)
+            {
+                suma += i;
+            }
+            Console.WriteLine($"\nSum = {suma}.\n");
 
+            // Task 14
+            int[] numbers = { 15, 25, 35 };
+
+            // a)
+            suma = 0;
+            foreach (int i in numbers)
+            {
+                suma += i;
+            }
+            if (suma % 2 == 0)
+            {
+                Console.WriteLine("\nIt's even number.");
+            }
+            else
+            {
+                Console.WriteLine("\nIt's odd number.");
+            }
+
+            // b)
+            suma = 0;
+            foreach (int i in numbers)
+            {
+                suma += i * i;
+            }
+            if (suma.ToString().Length == 5)
+            {
+                Console.WriteLine("\nIt's five-signed.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nIt's NOT five-signed.\n");
+            }
+
+            // Task 15
+            int[] elements = { 1, -3, 500, 0, 100, -31, 281 };
+
+            // a)
+            foreach (int i in elements)
+            {
+                if (i > 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+
+            // b)
+            foreach (int i in elements)
+            {
+                if (i <= 100)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine("\n");
+
+            // Task 16
+            int[] numbers2 = { 20, 30, 12, 910, 41, 0 };
+
+            // a)
+            foreach (int i in numbers2)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+
+            // b)
+            foreach (int i in numbers2)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine("\n");
+
+            // Task 17
+            foreach (int i in elements)
+            {
+                if (i > 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+            foreach (int i in elements)
+            {
+                if (i < 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            // Task 18
+            a = 5;
+            suma = 0;
+
+            // a)
+            foreach (int i in elements)
+            {
+                if (i <= 20)
+                {
+                    suma += i;
+                }
+            }
+            Console.WriteLine($"\n\nSum = {suma}");
+
+            // b)
+            suma = 0;
+            foreach (int i in elements)
+            {
+                if (i > a)
+                {
+                    suma += i;
+                }
+            }
+            Console.WriteLine($"Sum = {suma}");
+
+
+            // Task 19
+            suma = 0;
+            a = 3; b = 5;
+
+            // a)
+            foreach (int i in elements)
+            {
+                if (i % 2 != 0)
+                {
+                    suma += i;
+                }
+            }
+            Console.WriteLine($"\nSum = {suma}");
+
+            // b)
+            suma = 0;
+            foreach (int i in elements)
+            {
+                if (i % a != 0 || i % b != 0)
+                {
+                    suma += i;
+                }
+            }
+            Console.WriteLine($"Sum = {suma}");
+
+            // Task 20
+            suma = 0;
+            for (int i = 1; i < elements.Length; i+=2)
+            {
+                suma += elements[i];
+            }
+            Console.WriteLine($"\nSum = {suma}");
+
+            // Task 21
+            int[] height = new int[22];
+            for (int i = 0; i < height.Length; i++)
+            {
+                height[i] = rnd.Next(170, 200);
+            }
+            int counter = 0;
+            int r = 185;
+            foreach (int i in height)
+            {
+                if (i<=r)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"\nThere are {counter} pupils who are 185 cm high or shorter.\n");
+
+
+            // Task 22 (secret)
+            // Заданий масив. Поміняйте місцями: б) m-й і n-й елементи;
+
+            int[] arr = new int[10];
+
+            int n = rnd.Next(0, arr.Length);
+            int m = rnd.Next(0, arr.Length);
+            Console.WriteLine($"n={n} m={m}");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-163, 162);
+                Console.Write(arr[i] + " ");
+            }
+            SwapValues(ref arr[n], ref arr[m]); Console.WriteLine();
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
         }
+
+        /* ---------------------------------METHODS---------------------------------- */
 
         static string ToEUR(double uah)
         {
